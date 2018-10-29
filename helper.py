246b787394dd7d975d52dc3b2887a6f4f72e956e
xxx <xxx>
2018-10-28 20:21:47 -0400

    checkpoint

--- helper.py
+++ helper.py
@@ -8,10 +8,13 @@ from scipy.special import expit
 
 
 def gen_planar_samples(
-        *, complexity=10, noisiness=0.33, num=256, xylim=(-5, 5)):
+        *, complexity=10, noisiness=0.33, num=256, xylim=(-5, 5), seed=None):
     '''
     Generates random 2D features and labels based on plane waves.
     '''
+
+    if seed:
+        npr.seed(seed)
 
     # hack to work around late binding
     def xsin(amp, k, phi, xy):
@@ -53,7 +56,12 @@ def gen_planar_samples(
     return xys, y, amplitude
 
 
-def plot_decision_surface(fpred, xlim=(-5, 5), ylim=(-5, 5)):
+def plot_decision_surface(fpred, xlim=(-5, 5), ylim=(-5, 5),
+                          ax=None, with_data=None, size=(14, 8),
+                          with_true_surface=None):
+
+    if ax and with_true_surface:
+        raise ValueError('Cannot plot two surfaces with single passed axes!')
 
     xmn, xmx = xlim
     ymn, ymx = ylim
@@ -67,17 +75,52 @@ def plot_decision_surface(fpred, xlim=(-5, 5), ylim=(-5, 5)):
 
     # specialcase two classes, dirty but worx
     if len(Z.shape) == 2 and Z.shape[1] == 2:
-        Z = Z[:, 0]
+        Z = Z[:, 1]
 
     # print(np.c_[XX.ravel(), XX.ravel()].shape)
     # Z = np.apply_along_axis(fpred, 1, np.c_[XX.ravel(), XX.ravel()])
     Z = Z.reshape(XX.shape)
 
     # f = plt.figure()
-    ax = plt.gca()
+    if not ax:
+        # plt.figure()
+        if with_true_surface:
+            ax = plt.subplot(1, 2, 1)
+            ax_true = plt.subplot(1, 2, 2)
+        else:
+            ax = plt.gca()
+
+        # set size only if we own the axes
+        plt.gcf().set_size_inches(size)
+
     ax.contourf(XX, YY, Z, cmap=plt.cm.RdYlBu, vmin=0, vmax=1)
 
+    if with_data:
+        x, y = with_data
+        plot_red_blue(x, y, ax=ax)
+
+    if with_true_surface:
+        plot_decision_surface(with_true_surface,
+                              xlim=xlim, ylim=ylim, ax=ax_true)
+
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+    plt.tight_layout()
+
     # return f
+
+
+def plot_red_blue(x, y, ax=None):
+    # f = plt.figure()
+
+    if not ax:
+        plt.figure()
+        ax = plt.gca()
+
+    xred = x[y == 0]
+    xblue = x[y == 1]
+    ax.scatter(xred[:, 0], xred[:, 1], color='red')
+    ax.scatter(xblue[:, 0], xblue[:, 1], color='blue')
 
 
 if __name__ == '__main__':
